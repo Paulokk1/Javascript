df17ebb01e1ff5d4ca03be6d3d63f4feeebdcdd8
Paulo Filipe <felipepauloxd560@gmail.com>
2026-01-12 15:00:31 -0300

    Algumas coisas

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+
+console.log(`Você está a ${vel}km/h`)
+if (vel > 60) {
+    console.log('Você está acima da velocidade permitida, multado(a)')
+} 
+
+console.log(`Dirija com segurança`)
