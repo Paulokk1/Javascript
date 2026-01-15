7939922b7e8915596a6c2a38d0383df475bd3152
Paulo Filipe <felipepauloxd560@gmail.com>
2026-01-14 23:23:08 -0300

    atividades

---
+++ Aula013/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+
+do {
+    console.log('Comer fatia ' + c)
+    c++
+} while (c <= 6) {
+    console.log('Cabo fatia')
+}
+
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Comendo fatia')
+    c++
+}
+console.log('Cabo a pizaa :(')
+*/
