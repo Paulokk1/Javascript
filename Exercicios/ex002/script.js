473da92e35c97ccac9cc996e1c0a9296afaf7ef9
Paulo Filipe <felipepauloxd560@gmail.com>
2026-01-14 21:43:33 -0300

    mt coisa

--- Exercicios/ex002/script.js
+++ Exercicios/ex002/script.js
@@ -1,19 +1,56 @@
 function verificar() {
-    var agora = new Date()
-    var ano = agora.getFullYear
-    var idade1 = document.getElementById('iidade')
-    var idade = Number(idade1.value)
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('inasc')
+    var res = document.querySelector('div#res')
 
-    if (idade <= 1976) {
-    document.getElementById('res').innerHTML = 'Idoso'
-}   else if (idade >= 1977 && idade <= 2008) {
-    document.getElementById('res').innerHTML = 'Adulto'
-} else if (idade >= 2009 && idade < 2014) {
-    document.getElementById('res').innerHTML = 'Jovem'
-} else if (idade => 2026) {
-    document.getElementById('res').innerHTML = 'Valor incorreto'
-} else {
-    document.getElementById('res').innerHTML = 'Criança'
-}
-}
 
+    if (fano.value.length == 0 || fano.value >= ano) {
+        window.alert('[ERRO] verifique o ano e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('sex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.getElementById('foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && 11 > idade) {
+                //crianca
+                img.setAttribute('src', 'crianca-m.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
+        } else {
+            gênero = 'Mulher'
+            if (idade >= 0 && 11 > idade) {
+                //crianca
+                img.setAttribute('src', 'crianca-f.jpg')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adulto-f.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa-f.jpg')
+            }
+        }
+        res.innerHTML = `<p>Detectamos ${gênero} com ${idade} anos</p>`
+        res.appendChild(img)
+        img.style.width = '40%'
+        img.style.maxWidth = '400px'
+        img.style.borderRadius = '100%'
+        res.style.textAli
+    }
+
+
+
+}
--- testes/teste003/script.js
+++ testes/teste003/script.js
@@ -3,6 +3,12 @@ function verificar() {
     var ano = agora.getFullYear
     var idade1 = document.getElementById('iidade')
     var idade = Number(idade1.value)
+    //daqui pra cima é as idades
+    //daqui pra baixo o sexo
+    var sexoM = document.getElementById('isex1')
+    var sexoF = document.getElementById('isex2')
+
+
 
     if (idade <= 1976) {
     document.getElementById('res').innerHTML = 'Idoso'
@@ -10,7 +16,7 @@ function verificar() {
     document.getElementById('res').innerHTML = 'Adulto'
 } else if (idade >= 2009 && idade < 2014) {
     document.getElementById('res').innerHTML = 'Jovem'
-} else if (idade => 2026) {
+} else if (idade >= 2026) {
     document.getElementById('res').innerHTML = 'Valor incorreto'
 } else {
     document.getElementById('res').innerHTML = 'Criança'
